043cb23bb9ef5a1e24648fc1c09c6eda75ec1049
harpreet997 <harpreet97@gmail.com>
2023-04-25 16:46:14 +0530

    Made the necessary changes in loginController for user login

--- controllers/logincontroller.js
+++ controllers/logincontroller.js
@@ -4,25 +4,40 @@ const User = require('../models/usermodal');
 const login = async (req, res) => {
   try {
     const { email, password } = req.body
-    
-  if (!email || !password) {
-    res.status(400).json({ msg: "Please enter email and password" })
-  }
-  
-  else {
-    const id = new Date().getDate()
 
-    const token = jwt.sign({ id, email }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    })
+    if (!email || !password) {
+      res.status(400).json({ msg: "Please enter email and password" })
+    }
+    else if (email === "admin@test.com" && password === "admin1234") {
+      res.status(200).json({ msg: "Admin login successfully", role: "admin" })
+    }
+    else {
 
-    res.status(200).json({ token, role: "admin" })
-  }
+      User.find({ email: req.body.email }).exec()
+        .then(user => {
+          if (user.length < 1) {
+            res.status(401).json({ msg: "User not exists " })
+          }
+          else if (user[0].password !== req.body.password) {
+            res.status(401).json({ msg: "Password does not match" })
+          }
+          else {
+            const id = new Date().getDate()
+            const token = jwt.sign({ id, email }, process.env.JWT_SECRET, {
+              expiresIn: '30d',
+            })
+
+            res.status(200).json({ msg: "User login successfully", user: user, token, role: "user" })
+          }
+        })
+
+
+    }
   } catch (error) {
     console.log(error);
-    res.status(400).json({msg: "User not found"})
+    res.status(400).json({ msg: "User not found" })
   }
-  
+
 }
 
 
