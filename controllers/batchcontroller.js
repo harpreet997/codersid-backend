0033692fc5c16e3c9f400b78dc338d7a283d0a93
harpreet997 <harpreet97@gmail.com>
2023-04-27 14:36:30 +0530

    Changes in Batch controller

--- controllers/batchcontroller.js
+++ controllers/batchcontroller.js
@@ -9,7 +9,7 @@ const getAllBatches = asyncWrapper(async (req, res) => {
         res.status(401).json({msg: 'Unauthorize user'})
     }
     else{
-    const Batches = await Batch.find({}).sort({createdAt: -1})
+    const Batches = await Batch.find({})
     res.status(200).json({ Batches })
     }
 })
