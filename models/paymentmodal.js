adae125e4c82b7f46f12666dd265ddf58319ca37
harpreet997 <harpreet97@gmail.com>
2023-05-03 10:09:20 +0530

    Changes in Payment Modal for Storing Amount

--- models/paymentmodal.js
+++ models/paymentmodal.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const PaymentSchema = new mongoose.Schema({
-    studentname: {
+    StudentName: {
         type: String,
     },
     batchname: {
@@ -10,7 +10,7 @@ const PaymentSchema = new mongoose.Schema({
     course: {
         type: String,
     },
-    emailid: {
+    Email: {
         type: String,
     },
     contactdetails: {
