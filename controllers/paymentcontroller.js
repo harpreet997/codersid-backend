e2a1e00df8b11d4b641feff851d12fa32f89a362
harpreet997 <harpreet97@gmail.com>
2023-05-03 17:20:40 +0530

    Made changes in Payment Controller

--- controllers/paymentcontroller.js
+++ controllers/paymentcontroller.js
@@ -18,13 +18,7 @@ const getAllPayments = asyncWrapper(async (req, res) => {
 
 const createPayment = asyncWrapper(async (req, res) => {
     try {
-        const Payments = await Payment.find({})
-        const length = Payments.length;
-        let newPayment = {
-            ...req.body,
-            id: length + 1
-        }
-        const payment = await Payment.create(newPayment)
+        const payment = await Payment.create(req.body)
     if (!payment) {
         return next(createCustomError(`Please fill all the required fields`, 500))
     }
